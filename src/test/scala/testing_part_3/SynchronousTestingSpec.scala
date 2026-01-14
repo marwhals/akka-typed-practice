c0a9de6ebbd74e8a4646bdd7fccff172ebe0f277
marwhals <marjan.mubarok2012@gmail.com>
2026-01-14 19:01:52 +0000

    Child actors exercise

---
+++ src/test/scala/testing_part_3/SynchronousTestingSpec.scala
@@ -0,0 +1,8 @@
+package testing_part_3
+
+import akka.actor.testkit.typed.scaladsl.ScalaTestWithActorTestKit
+import org.scalatest.wordspec.AnyWordSpecLike
+
+class SynchronousTestingSpec extends ScalaTestWithActorTestKit with AnyWordSpecLike {
+
+}
