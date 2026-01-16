5958b26a385d74ec956745f1ea8eb4924a27cc02
marwhals <marjan.mubarok2012@gmail.com>
2026-01-16 17:47:05 +0000

    Scehdulers

---
+++ src/main/scala/infra_part_4/Schedulers.scala
@@ -0,0 +1,111 @@
+package infra_part_4
+
+import akka.actor.Cancellable
+import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.{ActorSystem, Behavior}
+import utils.ActorSystemEnhancements
+
+import scala.concurrent.duration.DurationInt
+
+object Schedulers {
+
+  object LoggerActor {
+    def apply(): Behavior[String] = Behaviors.receive { (context, message) =>
+      context.log.info(s"[${context.self.path}] Received: $message")
+      Behaviors.same
+    }
+  }
+
+  def demoScheduler(): Unit = {
+    val userGuardian = Behaviors.setup[Unit] { context =>
+      val loggerActor = context.spawn(LoggerActor(), "loggerActor")
+
+      context.log.info("[system] System starting")
+      context.scheduleOnce(1.second, loggerActor, "reminder")
+
+      Behaviors.empty
+    }
+
+    val system = ActorSystem(userGuardian, "DemoScheduler")
+
+    import system.executionContext // take care with futures
+    system.scheduler.scheduleOnce(2.seconds, () => system.terminate)
+  }
+
+  // timeout pattern
+  def demoActorWithTimeout(): Unit = {
+    val timeoutActor: Behavior[String] = Behaviors.receive { (context, message) =>
+      val schedule = context.scheduleOnce(1.second, context.self, "timeout")
+      message match {
+        case "timeout" =>
+          context.log.info("Stopping!")
+          Behaviors.stopped
+        case _ =>
+          context.log.info(s"Received $message")
+          Behaviors.same
+      }
+    }
+
+    val system = ActorSystem(timeoutActor, "TimeoutDemo")
+    system ! "trigger"
+    Thread.sleep(2000)
+    system ! "are you there"
+
+  }
+
+  /**
+   * Exercise: enhance the timeoutActor to reset its timer with every new message (except the "timeout" message)
+   *
+   */
+
+  object ResettingTimeoutActor {
+    def apply(): Behavior[String] = Behaviors.receive { (context, message) =>
+      context.log.info(s"Received: $message")
+      resettingTimeoutActor(context.scheduleOnce(1.second, context.self, "timeout"))
+    }
+
+    def demoActorResettingTimeout(): Unit = {
+      def resettingTimeoutActor(schedule: Cancellable): Behavior[String] = Behaviors.receive { (context, message) =>
+        // start the scheduler
+        var schedule = context.scheduleOnce(1.second, context.self, "timeout")
+
+        message match {
+          case "timeout" =>
+            context.log.info("Stopping!")
+            Behaviors.stopped
+          case _ =>
+            context.log.info(s"Received: $message")
+            // reset scheduler
+            schedule.cancel()
+            // start another scheduler
+            resettingTimeoutActor(context.scheduleOnce(1.second, context.self, "timeout"))
+            Behaviors.same
+        }
+      }
+    }
+  }
+
+  def demoActorResettingTimeout(): Unit = {
+    val userGuardian = Behaviors.setup[Unit] { context =>
+      val resettingTimeoutActor = context.spawn(ResettingTimeoutActor(), "resetter")
+
+      resettingTimeoutActor ! "start timer"
+      Thread.sleep(500)
+      resettingTimeoutActor ! "reset"
+      Thread.sleep(700)
+      resettingTimeoutActor ! "this should still be visible"
+      Thread.sleep(1200)
+      resettingTimeoutActor ! "This should not be visible"
+      Behaviors.empty
+    }
+
+    val system = ActorSystem(userGuardian, "DemoResettingTimeoutActor").withFiniteLifespan(4.seconds)
+    import system.executionContext
+    system.scheduler.scheduleOnce(3.seconds, () => system.terminate())
+
+  }
+
+  def main(args: Array[String]): Unit = {
+    demoActorResettingTimeout()
+  }
+}
---
+++ src/main/scala/utils/package.scala
@@ -0,0 +1,26 @@
+import akka.actor.TypedActor
+import akka.actor.TypedActor.context
+import akka.actor.typed.{ActorSystem, Behavior}
+import akka.actor.typed.scaladsl.Behaviors
+
+import scala.concurrent.duration.{DurationInt, FiniteDuration}
+
+package object utils {
+
+  object LoggerActor {
+    def apply[A](): Behavior[A] = Behaviors.receive { (context, message) =>
+      context.log.info(s"[${context.self.path}] Received: $message")
+      Behaviors.same
+    }
+  }
+
+  // use extension in Scala 3
+  implicit class ActorSystemEnhancements[A](system: ActorSystem[A]) {
+    def withFiniteLifespan(duration: FiniteDuration): ActorSystem[A] = {
+      import system.executionContext
+      system.scheduler.scheduleOnce(duration, () => system.terminate())
+      system
+    }
+  }
+
+}
