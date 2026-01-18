cc4c6eec952cd2a0dd013b8d6ae88b80ec199de0
marwhals <marjan.mubarok2012@gmail.com>
2026-01-18 17:36:03 +0000

    Ask pattern

---
+++ src/main/scala/pattern_part_5/AskDemo.scala
@@ -0,0 +1,83 @@
+package pattern_part_5
+
+import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.{ActorRef, ActorSystem, Behavior, Scheduler}
+import akka.util.Timeout
+import utils.ActorSystemEnhancements
+
+import scala.concurrent.duration.DurationInt
+import scala.concurrent.{ExecutionContext, Future}
+import scala.util.{Failure, Success}
+
+object AskDemo {
+
+  trait WorkProtocol
+
+  case class ComputationalTask(payload: String, replyTo: ActorRef[WorkProtocol]) extends WorkProtocol
+
+  case class ComputationalResult(Result: Int) extends WorkProtocol
+
+  object Worker {
+    def apply(): Behavior[WorkProtocol] = Behaviors.receive { (context, message) =>
+      message match {
+        case ComputationalTask(text, destination) =>
+          context.log.info(s"[worker] Crunching data for $text")
+          destination ! ComputationalResult(text.split(" ").length)
+          Behaviors.same
+        case _ => Behaviors.same
+      }
+    }
+  }
+
+  def askSimple(): Unit = {
+    // 1 - import the right package
+    import akka.actor.typed.scaladsl.AskPattern._
+
+    // 2 - set up some implicits
+    val system = ActorSystem(Worker(), "DemoAskSimple").withFiniteLifespan(5.seconds)
+    implicit val timeout: Timeout = Timeout(3.seconds)
+    implicit val scheduler: Scheduler = system.scheduler
+
+    // 3 - call the method
+    val reply: Future[WorkProtocol] = system.ask(ref => ComputationalTask("Trying the ask pattern, seems convoluted", ref))
+    //                                           ^ temporary actor     ^ message that gets sent to the worker == the same as the user guardian
+
+    // 4 - process the future
+    implicit val ec: ExecutionContext = system.executionContext
+    reply.foreach(println)
+
+  }
+
+  def askFromWithinAnotherActor(): Unit = {
+    val userGuardian = Behaviors.setup[WorkProtocol] { context =>
+      val worker = context.spawn(Worker(), "worker")
+
+      // 0 - define extra message that I should handle as a result of ask
+      case class ExtendedComputationalResult(count: Int, description: String) extends WorkProtocol
+      // 1 - set up the implicits
+      implicit val timeout: Timeout = Timeout(3.seconds)
+
+      // 2 - ask
+      context.ask(worker, ref => ComputationalTask("This ask pattern seems quite complicated", ref)) {
+        // Try[WorkProtocol] => WorkProtocol message that will be sent to the actor who is calling the worker protocol
+        case Success(ComputationalResult(count)) => ExtendedComputationalResult(count, "This is pretty damn hard")
+        case Failure(ex) => ExtendedComputationalResult(-1, s"Computation failed: $ex")
+      }
+
+      // 3 - handle the result (messages) from the ask pattern
+      Behaviors.receiveMessage {
+        case ExtendedComputationalResult(count, description) =>
+          context.log.info(s"ask and ye shall receive: $description - $count")
+          Behaviors.same
+        case _ => Behaviors.same
+      }
+    }
+
+    ActorSystem(userGuardian, "DemoAskConvoluted").withFiniteLifespan(5.seconds)
+  }
+
+  def main(args: Array[String]): Unit = {
+    askFromWithinAnotherActor()
+  }
+
+}
